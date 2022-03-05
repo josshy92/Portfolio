ba455af3ef13a0a922dee43764297552fe422263
Josshy <josshy92@hotmail.com>
2022-03-05 10:04:07 -0600

    removed unused code and made sure the link worked

--- src/components/contactInfo/ContactInfo.js
+++ src/components/contactInfo/ContactInfo.js
@@ -3,40 +3,6 @@ import emailjs from '@emailjs/browser';
 import './Contactinfo.css'
 
 
-// export default function Contactinfo() {
-//   const form = useRef();
-
-//   const sendEmail = (e) => {
-//     e.preventDefault();
-
-
-//     emailjs.sendForm('service_jcywjgv', 'template_ysgjhpo', form.current, 'lE1hCSWcbPFOxCMj6')
-//       .then((result) => {
-//         console.log("yay, it worked!", result);
-//       }, (error) => {
-//         console.log("oh no!", error.text);
-//       });
-//   };
-
-//   return (
-//     <div className='parentForm'>
-//       <h2 className='contact'>Contact Me</h2>
-
-//       <form className='form' ref={form} onSubmit={sendEmail}>
-//         <label className='contact' >Name</label>
-//         <input  type="text" name="user_name" required />
-//         <label className='contact' >Email</label>
-//         <input type="email" name="user_email" required/>
-//         <label className='contact'>Message</label>
-//         <textarea name="message" required/>
-//         <button onClick={Contactinfo} type="submit" value="Send" >Submit</button>
-//       </form>
-//     </div>
-//   );
-
-// };
-
-
 const Contactinfo = () => {
   const form = useRef();
 
